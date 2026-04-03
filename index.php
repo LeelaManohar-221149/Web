0129efac9ec89fd4e9b1909d37a6f2e3c2c4cb5c
MANOHAR  MATHURTHI <n221149@rguktn.ac.in>
2026-04-03 21:15:56 +0530

    chnages made inindex.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
 
 <!-- GOOGLE FONT -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="script.css">
 
 
 </head>
